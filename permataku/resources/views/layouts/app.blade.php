dabd7c848fedd215f67035e6a0a63cbe1f86b8ed
MFahmi19 <fahmitb70@gmail.com>
2019-04-20 16:21:42 +0700

    Menambahkan Icon dan Logo

--- permataku/resources/views/layouts/app.blade.php
+++ permataku/resources/views/layouts/app.blade.php
@@ -4,9 +4,9 @@
     <!-- Meta-Information -->
     <title>Permata</title>
 
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
     <link rel="images/logo/logo.png" rel="icon" />
+    <link rel="icon" type="image/png" sizes="56x56" href="images/fav-icon/permata.png">
+    
     <meta charset="utf-8">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
